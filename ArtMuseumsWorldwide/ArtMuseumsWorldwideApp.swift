8934e6824a06808dd99760849e66bb7bbe251531
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-06-26 13:29:42 +0900

    Initial Commit

---
+++ ArtMuseumsWorldwide/ArtMuseumsWorldwideApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtMuseumsWorldwideApp.swift
+//  ArtMuseumsWorldwide
+//
+//  Created by Jinwook Kim on 2023/06/26.
+//
+
+import SwiftUI
+
+@main
+struct ArtMuseumsWorldwideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
